5088847e050965d5f55709685526857a7ededdcd
mayararysia <mayara.ryzia@gmail.com>
2020-07-27 16:02:03 -0300

    comment in class

--- src/main/java/com/rysia/conferencedemo/configuration/PersistenceConfiguration.java
+++ src/main/java/com/rysia/conferencedemo/configuration/PersistenceConfiguration.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference");
-        builder.username("");
-        builder.password("");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference");
+//        builder.username("");
+//        builder.password("");
+//        return builder.build();
+//    }
 }
